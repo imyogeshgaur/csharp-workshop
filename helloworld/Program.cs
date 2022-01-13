2ff784a0ecac92ce9ec5239f9ed4484b21961e31
Mukeshgaur12 <yogeshgaur1995@gmail.com>
2022-01-13 16:35:36 +0530

    Done

---
+++ helloworld/Program.cs
@@ -0,0 +1,5 @@
+﻿//Readline is Used to Read a Particular String From the  console with additional newline.
+//WriteLine is Used to write the psrticular sting on the console with additional newline.
+Console.WriteLine("Hello, World!");
+string s = Console.ReadLine();
+Console.WriteLine("The input is : "+ s);
